aeef47c1a41c9269088294c0718d3b338607c865
Alexandr <torasov.alexandr@gmail.com>
2021-10-04 06:37:09 +0300

    transferred Pathsreducers in own folder

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -4,42 +4,12 @@ import { MainPage } from "../pages/MainPage";
 import { ProfilePage } from "../pages/ProfilePage/Index";
 
 import { ReduxExample } from "../pages/ReduxExample";
-
-const reducer = (
-  previousValue: Function | string,
-  currentValue: Function | string,
-  index: number,
-  Array: Array<Function | string>
-) => {
-  let a =
-    typeof currentValue === "function"
-      ? currentValue()
-      : currentValue.toString();
-
-  return index > 0 ? previousValue + "/" + a : a;
-};
-
-const reducePath = (...args: Array<Function | string>): string => {
-  const path = args.reduce(reducer, "");
-  if (path.length === 0) {
-    return "/" + path;
-  }
-  if (typeof path == "string" && path[1] === "/") {
-    return path.slice(1);
-  }
-  if (typeof path == "string") return path;
-  return "";
-};
-
-const reduceHomePath = (path = "") => reducePath(path);
-
-const reduceProfilePath = (path = "profile") =>
-  reducePath(reduceHomePath, path);
-
-const reduceExamplePath = (path = "example") =>
-  reducePath(reduceHomePath, path);
-
-const reduceChatIdPath = (path = ":chatId") => reducePath(reduceHomePath, path);
+import {
+  reduceProfilePath,
+  reduceHomePath,
+  reduceExamplePath,
+  reduceChatIdPath,
+} from "./pathReducers";
 
 export const useRoutes = (isAuthenticated: boolean) => {
   return (
